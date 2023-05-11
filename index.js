473f602e640e1b1f1b395951304222dfc8a81daf
sonalijain20 <sonalijn20@gmail.com>
2023-05-11 17:48:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express= require('express');
+require('dotenv').config();
+
+const router= require('./routes/index')
+const port= process.env.PORT || 1111;
+const app=express();
+
+
+app.use('/api', router);
+
+//return 404 if no API is found
+app.use('*', (req, res) => {
+    return res.status(404).json({
+        statusCode: 404,
+        messgae: 'Not Found'
+    })
+})
+
+app.listen(port, function(){
+    console.log("Listening to port: ", port);
+})
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const express = require('express');
+const routes = express.Router();
+module.exports=routes
